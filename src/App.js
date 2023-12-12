f0f3a47ed9e945b4408968ca43291fdc3091559d
Samir Ali <82404683+samir142@users.noreply.github.com>
2023-12-12 13:41:14 +0200

    Carousel in home page finished

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Navigation from './components/Navigation';
 import AdminView from './views/AdminView';
 import ReviewsAdmin from './views/ReviewsAdmin';
+import Home from './views/Home';
 
 function App() {
   return (
     <>
       <Navigation />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/adminSecret" element={<AdminView />} />
         <Route path="/adminSecret/reviews" element={<ReviewsAdmin />} />
       </Routes>
