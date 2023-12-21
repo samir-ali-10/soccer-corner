653666155e2187806981e03f4426da6df44f3659
Samir Ali <82404683+samir142@users.noreply.github.com>
2023-12-21 10:57:24 +0200

    Fixing issues

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -5,11 +5,13 @@ import image2 from "../images/carousel_2.jpeg"
 import image3 from "../images/carousel_3.jpeg"
 import { Container } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 
 export default function Home() {
     return (
         <div className='home_page'>
-            <Carousel fade>
+            <Carousel>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
@@ -45,43 +47,101 @@ export default function Home() {
                 </Carousel.Item>
             </Carousel>
             <Container>
-                <div className="about_section">
-                    <h2 className='mb-4'>About Us</h2>
-                    <p>Our goal is the bring to your a very well developed and quality product in the field of football and sport. By reviving the spirit of collaboration and sportiveness</p>
-                </div>
-
                 <div className="new_collection">
                     <p>new collection</p>
                     <div className="new_collection_container">
-                        <div className="item half one_sm">
-                            <NavLink to="/products/alahly">
+                        <div className="item">
+                            <NavLink to="/products/footballJerseys">
                                 <img src={image1} alt="Image1" />
-                                <div className="category">Alahly</div>
+                                <div className="category">Football Jerseys</div>
                             </NavLink>
                         </div>
-                        <div className="item half">
-                            <NavLink to="/products/zamalek">
+                        <div className="item">
+                            <NavLink to="/products/sportsWear">
                                 <img src={image2} alt="Image1" />
-                                <div className="category">Zamalek</div>
+                                <div className="category">Sports Wear</div>
                             </NavLink>
                         </div>
                         <div className="item">
-                            <NavLink to="/products/paris">
+                            <NavLink to="/products/studs_turfs">
                                 <img src={image3} alt="Image1" />
-                                <div className="category">Paris</div>
+                                <div className="category">Studs & Turfs</div>
                             </NavLink>
                         </div>
                         <div className="item">
-                            <NavLink to="/products/barcelona">
+                            <NavLink to="/products/others">
                                 <img src={image1} alt="Image1" />
-                                <div className="category">Barcelona</div>
+                                <div className="category">Others</div>
                             </NavLink>
                         </div>
+                    </div>
+                </div>
+
+                <div className="best_sellers">
+                    <h2>Best Sellers</h2>
+                    <div className="all_products">
+                        <button>All Products</button>
+                    </div>
+                    <div className="slider_container d-flex">
                         <div className="item">
-                            <NavLink to="/products/realMadrid">
-                                <img src={image3} alt="Image1" />
-                                <div className="category">Real Madrid</div>
-                            </NavLink>
+                            <div className="image">
+                                <img src={image1} alt="image1" />
+                            </div>
+                            <div className="info">
+                                <div className="name">ahly t-shirt one</div>
+                                <div className="inner_info d-flex justify-content-between">
+                                    <div className="price">300EGP</div>
+                                    <div className="add_cart"><FontAwesomeIcon icon={faCartPlus} /></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="image">
+                                <img src={image2} alt="image2" />
+                            </div>
+                            <div className="info">
+                                <div className="name">ahly t-shirt one</div>
+                                <div className="inner_info d-flex justify-content-between">
+                                    <div className="price">300EGP</div>
+                                    <div className="add_cart"><FontAwesomeIcon icon={faCartPlus} /></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="image">
+                                <img src={image3} alt="image3" />
+                            </div>
+                            <div className="info">
+                                <div className="name">ahly t-shirt one</div>
+                                <div className="inner_info d-flex justify-content-between">
+                                    <div className="price">300EGP</div>
+                                    <div className="add_cart"><FontAwesomeIcon icon={faCartPlus} /></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="image">
+                                <img src={image2} alt="image1" />
+                            </div>
+                            <div className="info">
+                                <div className="name">ahly t-shirt one</div>
+                                <div className="inner_info d-flex justify-content-between">
+                                    <div className="price">300EGP</div>
+                                    <div className="add_cart"><FontAwesomeIcon icon={faCartPlus} /></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="image">
+                                <img src={image2} alt="image1" />
+                            </div>
+                            <div className="info">
+                                <div className="name">ahly t-shirt one</div>
+                                <div className="inner_info d-flex justify-content-between">
+                                    <div className="price">300EGP</div>
+                                    <div className="add_cart"><FontAwesomeIcon icon={faCartPlus} /></div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
