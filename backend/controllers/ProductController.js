1b05d59b02bd4a0f74d1bc159c9918fe172d8fb2
yossifdiaa <yossifdiaa4@gmail.com>
2023-12-30 10:46:38 +0200

    get functions and edit edit product

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -51,18 +51,48 @@ exports.getBySize = (req, res, next) => {
     });
 };
 
-// exports.getByModelAndSize = (req , res, next) => {
-//   const size = req.params.size;
-//   const model = req.params.model;
-//   ProductModel.find({model , size })
-//   .then(products => {
-//     res.json(products)
-//     console.log(products);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   })
-// }
+
+exports.getCollectionAndModel = (req , res, next) => {
+  const collectionName = req.params.collectionName;
+  const model = req.params.model;  
+  ProductModel.find({ model , collectionName})
+  .then(products => {
+    res.json(products)
+    console.log(products);
+  })
+  .catch(err => { 
+    console.log(err);
+  })
+}
+
+
+exports.getCollectionAndSize = (req , res, next) => {
+  const collectionName = req.params.collectionName;
+  const size = req.params.size;
+  ProductModel.find({ size , collectionName})
+  .then(products => {
+    res.json(products)
+    console.log(products);
+  })
+  .catch(err => { 
+    console.log(err);
+  })
+}
+
+
+exports.getCollectionAndModelAndSize = (req , res, next) => {
+  const collectionName = req.params.collectionName;
+  const size = req.params.size;
+  const model = req.params.model;
+  ProductModel.find({model , size , collectionName})
+  .then(products => {
+    res.json(products)
+    console.log(products);
+  })
+  .catch(err => { 
+    console.log(err);
+  })
+}
 
 
 
--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -7,7 +7,9 @@ router.get("/api/products", ProductController.getProducts); // get all products
 router.get("/api/products/collection/:collectionName",ProductController.getCollection); // get collection
 router.get("/api/products/code/:code",ProductController.getSingleProduct); // get single product
 router.get("/api/products/size/:size", ProductController.getBySize); // get by size
-// router.get("/api/products/modelAndSize/:model/:size",ProductController.getByModelAndSize); // get by model and size 
+router.get("/api/products/collection/:collectionName/model/:model",ProductController.getCollectionAndModel); // get collection and  model 
+router.get("/api/products/collection/:collectionName/size/:size",ProductController.getCollectionAndSize); // get collection and size 
+router.get("/api/products/collection/:collectionName/model/:model/size/:size",ProductController.getCollectionAndModelAndSize); // get collection , model and size 
 // router.get('/api/products/edit-product/:code' , ProductController) //
 
 // => POST
