9b2042a9b65b4fc9af367c63d4d2223650846f99
Samir Ali <82404683+samir142@users.noreply.github.com>
2024-01-15 21:50:42 +0200

    Initialized new order

--- src/views/admin/New.jsx
+++ src/views/admin/New.jsx
@@ -31,28 +31,56 @@ export default function New() {
                             <h1>There are no new orders</h1>
                         </div>
                         :
-                        <div className="new_orders_container">
-                            {
-                                cart.map(item =>
-                                    <div key={item._id} className="new_item">
-                                        <div className="info d-flex justify-content-around align-items-center">
-                                            <div className="image">
-                                                <img src={image3} alt="image1" />
-                                            </div>
-                                            <div className="content">
+                        <>
+                            <div className="types d-flex justify-content-around mt-5">
+                                <p className='image'>Image</p>
+                                <p className='code'>Code</p>
+                                <p className='size'>Size</p>
+                                <p className='quantity'>Quantity</p>
+                                <p className='price'>Price</p>
+                                <p className='name'>Name</p>
+                                <p className='phone'>Phone</p>
+                                <p className='area'>Area-Zone</p>
+                                <p className='address'>Address</p>
+                            </div>
+                            <div className="new_orders_container">
+                                {
+                                    cart.map(item =>
+                                        <div key={item._id} className="new_item">
+                                            <div className="info d-flex justify-content-around align-items-center">
+                                                <div className="image">
+                                                    <img src={image3} alt="image1" />
+                                                </div>
                                                 <div className="code">
                                                     <h5>{item.code}</h5>
                                                 </div>
-                                                <p>{item.size.toUpperCase()}</p>
-                                            </div>
-                                            <div className="price">
-                                                {item.price}EGP
+                                                <div className="size">
+                                                    S
+                                                </div>
+                                                <div className="quantity">
+                                                    8
+                                                </div>
+                                                <div className="price">
+                                                    {item.price}EGP
+                                                </div>
+                                                <div className="name">
+                                                    Samir
+                                                </div>
+                                                <div className="phone">
+                                                    01097621363
+                                                </div>
+                                                <div className="area_zone">
+                                                    Giza, Egypt
+                                                </div>
+                                                <div className="address">
+                                                    El Rehab St., Giza Governorate, Egypt
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                )
-                            }
-                        </div>
+                                    )
+                                }
+                            </div>
+                        </>
                 }
             </Container>
         </div>
